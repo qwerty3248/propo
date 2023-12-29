e9af31b929bc14cbd0371742ec21e6616b288a84
Jesus Losada Arauzo <115494368+qwerty3248@users.noreply.github.com>
2023-12-29 15:50:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let sí=document. getElementById("sí");
+sí.addEventListener("click",function(e){
+	alert("Sabía que aceptarías😺😺");
+} );
+let no=document. getElementById("no");
+no.addEventListener("mousemove",function(e){
+	let x = Math.round( Math.random() * 93 );
+	let y = Math.round( Math.random() * 93 );
+	no.style.left = x+"%";
+  no.style.top = y+"%";
+} );
